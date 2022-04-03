0de20e5fc5e842881bdb1155e9b964254e6cb4d9
Ade Mawan <ademawan1210@gmail.com>
2022-04-03 22:57:35 +0700

    remove list task

---
+++ src/components/SideBar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import  { Col } from 'react-bootstrap'
+
+export default class SideBar extends Component {
+  render() {
+    return (
+        <Col md={2} mt={2}>test</Col>
+
+    )
+  }
+}
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,6 @@
 
-import React, { useState, useEffect } from 'react';
-import { Col, Card, Row,Button , Modal} from "react-bootstrap";
+import React, { Component,useState, useEffect } from 'react';
+import { Row, Container} from "react-bootstrap";
 
 
 
@@ -8,123 +8,21 @@ import { useHistory } from 'react-router';
 
 import axios from 'axios';
 import { API_URL } from '../utils/constants';
+import SideBar from '../components/SideBar';
 
-function Dashboard() {
-
-
-    const [user, setUser] = useState({});
-    const [tasks, setTasks] = useState([]);
-
-
-    const history = useHistory();
-
-    const token = localStorage.getItem("token");
-
- 
-    const fetchData = async () => {
-
-        
-        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-
-        await axios.get('https://rogerdev-beckend-todolist.herokuapp.com/users/me')
-        .then((response) => {
-
-
-            setUser(response.data.data);
-        })
-        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
-
-        await axios.get('https://rogerdev-beckend-todolist.herokuapp.com/users/me/tasks/today')
-        .then((response) => {
-
-      
-            setTasks(response.data.data);
-        })
-    }
-
-    useEffect(() => {
-
- 
-        // if(!token) {
-
- 
-        //     history.push('/login');
-        // }
-        
- 
-        fetchData();
-    }, []);
-
- 
-
-
-   
-
-    const [show, setShow] = useState(false);
-
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
+export default class Dashboard extends Component {
+  render() {
     return (
-
-        
-
-        <div className="container" style={{ marginTop: "10px" }}>
-            <div className="row justify-content-center">
-                <div className="col-md-12">
-                    <div className="card border-0 rounded shadow-sm">
-                        <div className="card-body">
-                            SELAMAT DATANG <strong className="text-uppercase">{user.name}</strong>
-                            <br/>
-                            This is your schedule for today..
-                            <hr />
-                            <Row className="overflow-auto menu">
-                            {tasks &&
-                             tasks.map((task) => (
-                            <Col md={4} xs={6} className="mb-4">
-                               
-                                <Card className="shadow">
-                                   
-                                    <Card.Body>
-                                    <Card.Title>{task.title}</Card.Title>
-                                    <Card class="Text">Status : {task.status}</Card>
-                                    <Card class="Text">Priority : {task.priority}</Card>
-                                    <Card class="Text">Time : {task.todo_date_time}</Card>
-                                 
-                                    <Button variant="primary" onClick={handleShow}>
-                                        Launch demo modal
-                                    </Button>
-                                    <Modal show={show} onHide={handleClose} animation={false}>
-                                        <Modal.Header closeButton>
-                                        <Modal.Title>{task.title}</Modal.Title>
-                                        </Modal.Header>
-                                        <Modal.Body>{task.note}</Modal.Body>
-                                        <Modal.Footer>
-                                        <Button variant="secondary" onClick={handleClose}>
-                                            Close
-                                        </Button>
-                                        
-                                        </Modal.Footer>
-                                    </Modal>
-                                    </Card.Body>
-                                </Card>
-
-                            </Col>
-                             ))}
-                             </Row>
-                           
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-        
+      <div className='mt-3'>
+          <Container fluid>
+          <Row>
+              <SideBar/>
+              
+          </Row>
+          </Container>
+      </div>
     )
-    
-      
-    
-
+  }
 }
 
-export default Dashboard;
+
